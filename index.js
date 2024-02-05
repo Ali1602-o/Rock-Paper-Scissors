bace1444f508d32173f863a7ae03d7b49639ba82
Ali <82242191+Ali1602-o@users.noreply.github.com>
2024-02-05 20:07:19 +0100

    added alert popups to improve user experience and made the input validation case insensitive

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const INVALID_INPUT_MSG = 'Invalid Input, Game over!';
 
 //GAME SETTINGS
-const WIN_MSG = 'Congrats, You Won !, Reload to play more !';
+const WIN_MSG = 'Congrats, You Won ! Reload to play more !';
 const LOSE_MSG = 'You Lost :(, Reload to try again !';
 const DRAW_MSG = "It's a Draw, Reload to try again !";
-const PLAYER_CHOICES = ['Rock', 'Paper', 'Scissors'];
+const PLAYER_CHOICES = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {    
     let randomIndex = Math.floor(Math.random() * 3); // random * (max (2) - min (0)) + min (0)
@@ -33,44 +33,63 @@ function playGame() {
 
     let playerScore = 0;
     let computerScore = 0;
+    
 
     for (var i = 0; i < 5; i++ ) {
         console.log(`------- Round ${i + 1} -------`);
 
-
-        const playerSelection = prompt('Write your choice : Rock | Paper | Scissors');
+        const playerSelection = prompt(`
+        Write your choice : Rock | Paper | Scissors :
+                    ( YOU ) ${playerScore}  - ${computerScore} ( COMPUTER )
+                                Rounds Left : ${5 - i} 
+        `);
         const computerSelection = getComputerChoice();
+        
         
         console.log("Your choice : " + playerSelection);
         console.log("Computer choice : " + computerSelection);
         
-        let result = playRound(playerSelection, computerSelection);
+        let result = playRound(playerSelection.toLowerCase(), computerSelection);
 
         if (result == WIN_MSG) {
             console.log('You Won this round !');
+            
+            alert('You Won this round ! Computer choose ' + computerSelection);
             playerScore++;
         } else if (result == LOSE_MSG) {
             console.log('You lost this round !');
+            alert('You lost this round ! Computer choose ' + computerSelection);
             computerScore++;
         } else if(result == DRAW_MSG) {
             console.log('Draw');
+            alert('Draw, Computer choose ' + computerSelection);
         } else {
             console.log(result);
+            alert( result + `
+            
+            
+                Reload the page to restart the game !`);
             break;
         }
     }
     
     console.log(`------- FINAL SCORE -------`);
     console.log(`( YOU ) ${playerScore}  - ${computerScore} ( COMPUTER )`);
-    
+    let result = `
+    ( YOU ) ${playerScore}  - ${computerScore} ( COMPUTER )
+    `;
     if (i == 5) {
         if (playerScore == computerScore) {
             console.log (DRAW_MSG);
+            result += DRAW_MSG;
         } else if (playerScore > computerScore) {
             console.log (WIN_MSG);
+            result += WIN_MSG;
         } else {
             console.log (LOSE_MSG);
+            result += LOSE_MSG;
         }
+        alert(result);
     }
     console.log(`<==========================================>`);
 }
